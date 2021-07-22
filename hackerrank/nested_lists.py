11d51a77b1014ee89af6e041a58917587776f01b
AnkitSuwal <ankitsuwal@gmail.com>
2021-07-22 14:33:03 +0530

    modified code

--- hackerrank/nested_lists.py
+++ hackerrank/nested_lists.py
@@ -6,10 +6,12 @@ def input_(num):
 
 
 def second_lowest(data):
-    pass
+    second_ = sorted(list(set([marks for name, marks in data])))[1]
+    print('\n'.join([name for name, marks in sorted(data) if marks == second_]))
 
 
 if __name__ == "__main__":
     iter_val = int(input())
     data = input_(iter_val)
+    # data = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
     second_lowest(data)
